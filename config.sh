1f0e288905d4362f3cd668a70bc354610d50381e
roman009 <valeriu.buzila@gmail.com>
2019-03-27 23:24:21 +0200

    Delete config.sh

--- config.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-cp -f .env ./backend-js/
-cp -f .env ./backend-php/
-
-docker-compose up --build -d
-
-docker exec moneywaster_backend-php-nginx_1 php composer install -n -vvv
-docker exec moneywaster_backend-php-nginx_1 ./bin/console doctrine:migrations:migrate -n
